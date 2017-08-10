156277c176510eb6e8b210cba81c2064ad4e3451
Mario <m_reder10@cs.uni-kl.de>
2017-08-10 14:20:57 +0200

    updated write channel

--- src/Bot.js
+++ src/Bot.js
@@ -12,7 +12,7 @@ const nightmare = Nightmare({
 })
 
 const readChannelId = '313398928890527746'
-const writeChannelId = '345136707861086220'
+const writeChannelId = '345179574033842177'
 const xlsxUrl = 'https://onedrive.live.com/view.aspx?resid=B92275A9BF72C170!40986&ithint=file%2cxlsx&app=Excel&authkey=!AJDJRXahXJTfaUU'
 
 export default class Bot {
